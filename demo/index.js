782d6439bc408e087e36a715068661192b27b582
JAVASCRIPT2 <ronakjatapara76@gmail.com>
2025-04-10 16:53:17 +0530

    Added

---
+++ demo/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const port = 1008
+const path = require("path")
+
+const app = express()
+
+app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+app.listen(port,(err)=>{
+    err ? console.log(err) : console.log("server started on port " + port);
+})
