b38fda19f87fa1f9f0bf376c05fd27e30e74ecf4
JAVASCRIPT2 <ronakjatapara76@gmail.com>
2025-04-22 19:12:18 +0530

    Addedd

--- Admin-Panel(Day-1)/controller/ctl.js
+++ Admin-Panel(Day-1)/controller/ctl.js
@@ -1,10 +1,23 @@
 
 
 // const mongoose = require("./")
+const schema = require("../model/firstSchema")
 
 module.exports.dashboard = (req,res)=>{
     res.render("dashboard")
 }
 module.exports.addAdmin = (req,res)=>{
     res.render("addAdmin")
+}
+module.exports.add = async(req,res)=>{
+    await schema.create(req.body).then(()=>{
+        res.redirect("/")
+    })
+    
+}
+module.exports.viewAdmin = async(req,res)=>{
+    await schema.find({}).then((admin)=>{
+        res.render("viewAdmin",{admin})
+    })
+    // res.render("viewAdmin")
 }
--- Admin-Panel(Day-1)/routes/route.js
+++ Admin-Panel(Day-1)/routes/route.js
@@ -6,5 +6,7 @@ const ctl = require("../controller/ctl")
 
 router.get("/",ctl.dashboard)
 router.get("/addAdmin",ctl.addAdmin)
+router.post("/addAdmin",ctl.add)
+router.get("/viewAdmin",ctl.viewAdmin)
 
 module.exports = router
