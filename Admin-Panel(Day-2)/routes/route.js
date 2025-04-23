48d99904c4bb0bec9fa0e1eb8a0b2ea6e8444113
JAVASCRIPT2 <ronakjatapara76@gmail.com>
2025-04-23 16:49:08 +0530

    added

---
+++ Admin-Panel(Day-2)/routes/route.js
@@ -0,0 +1,18 @@
+const express = require("express")
+
+const router = express.Router();
+
+const ctl = require("../controller/ctl")
+const upload = require("../middleware/multer")
+
+router.get("/",ctl.dashboard)
+router.get("/addAdmin",ctl.addAdmin)
+router.post("/addAdmin",upload,ctl.add)
+router.get("/viewAdmin",ctl.viewAdmin)
+router.get("/deleteData",ctl.delete)
+router.get("/editdata",ctl.edit)
+router.post("/updateAdmin",upload,ctl.update)
+
+
+
+module.exports = router
