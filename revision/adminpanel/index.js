66828453ef136f3409392787112b30bbf0afd7f9
JAVASCRIPT2 <ronakjatapara76@gmail.com>
2025-04-25 17:40:40 +0530

    Added

--- revision/adminpanel/index.js
+++ revision/adminpanel/index.js
@@ -4,10 +4,13 @@ const path = require("path")
 
 const app = express();
 const db = require("./config/db")
+const cookie = require("cookie-parser")
+
 app.set("view engine","ejs")
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,"public")))
 app.use("/uploads",express.static(path.join(__dirname,"uploads")))
+app.use(cookie())
 
 
 app.use("/",require("./routes/route"))
--- revision/adminpanel/routes/route.js
+++ revision/adminpanel/routes/route.js
@@ -4,12 +4,16 @@ const router = express.Router()
 const ctl = require("../controller/ctl")
 const multer = require("../middleware/multer")
 
-router.get("/",ctl.dashboard)
+router.get("/",ctl.login)
+router.post("/login",ctl.loginAdmin)
+router.get("/logout",ctl.logout)
+router.get("/dashboard",ctl.dashboard)
+
 router.get("/addAdmin",ctl.addAdmin)
 router.post("/addAdmin",multer,ctl.add)
 router.get("/deleteData",ctl.delete)
-router.get("/editData",multer,ctl.edit)
-
+router.get("/editData",ctl.edit)
+router.post("/updateAdmin",multer,ctl.update)
 router.get("/viewAdmin",ctl.viewAdmin)
 
 
