f9f8a3cd68309d3ed457c27c9da0a2d52b5e7a86
JAVASCRIPT2 <ronakjatapara76@gmail.com>
2025-05-06 18:59:41 +0530

    added

---
+++ passportJs(day-4)/index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const port  = 1008
+const path = require("path")
+
+const app = express()
+const db = require("./config/db")
+const cookie = require("cookie-parser")
+const passport = require("passport")
+const session = require("express-session")
+
+app.set("view engine","ejs")
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,"public")))
+app.use("/uploads",express.static(path.join(__dirname,"uploads")))
+app.use(cookie())
+
+app.use(session({
+    name:"ronak",
+    secret:"rnw",
+    resave:true,
+    saveUninitialized:false,
+    cookie:{maxAge:100*100*60} // for 10 minute
+}))
+
+app.use(passport.session())
+app.use(passport.initialize())
+
+app.use("/",require("./routes/route"))
+
+app.listen(port,(err)=>{
+    err ? console.log(err) : console.log("server started on port : "+port);
+})
